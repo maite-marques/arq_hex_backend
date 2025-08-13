9594b263b8f48b8412e5754104c1639c48668386
Maitê Caetano <maitecaetano@MacBook-Pro-de-Maite.local>
2025-08-12 23:48:04 -0300

    feat: add product use case

--- src/external/api/LoginUsuarioController.ts
+++ src/external/api/LoginUsuarioController.ts
@@ -5,18 +5,15 @@ import ProvedorJwt from './ProvedorJwt';
 export default class LoginUsuarioController {
   constructor(servidor: Express, casoDeUso: LoginUsuario){
     servidor.post('/api/usuarios/login', async(req, res) => {
-      try {
-        const usuario = await casoDeUso.executar({
-          email: req.body.email,
-          senha: req.body.senha
-        })
+        try {
+          const usuario = await casoDeUso.executar({
+            email: req.body.email,
+            senha: req.body.senha
+          })
 
-        const provedorJwt = new ProvedorJwt(process.env.JWT_SECRET!)
+          const provedorJwt = new ProvedorJwt(process.env.JWT_SECRET!)
 
-        res.status(200).send({
-          usuario,
-          token: provedorJwt.gerar(usuario)
-        })
+        res.status(200).send(provedorJwt.gerar(usuario))
       } catch(erro: any) {
         res.status(400).send(erro.message)
       }
