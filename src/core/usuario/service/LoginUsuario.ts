3aaac3e42e3926cad6ae84c16650f479a2b28916
Maitê Caetano <maitecaetano@MacBook-Pro-de-Maite.local>
2025-08-12 22:20:19 -0300

    feat: implementing user login use case

---
+++ src/core/usuario/service/LoginUsuario.ts
@@ -0,0 +1,37 @@
+import CasoDeUso from '@/core/shared/CasoDeUso';
+import Usuario from '../model/Usuario';
+import RepositorioUsuario from './RepositorioUsuario';
+import Erros from '@/core/shared/Erros';
+import ProvedorCriptografia from './ProvedorCriptografia';
+
+export type LoginUsuarioEntrada = {
+  email: string;
+  senha: string;
+}
+
+export type LoginUsuarioSaida = {
+  usuario: Usuario;
+  token: string
+}
+
+export default class LoginUsuario implements CasoDeUso<LoginUsuarioEntrada, LoginUsuarioSaida> {
+  constructor(
+    private repositorio: RepositorioUsuario,
+    private provedorCripto: ProvedorCriptografia
+  ){}
+
+  async executar(entrada: LoginUsuarioEntrada): Promise<LoginUsuarioSaida> {
+    const usuarioExistente = await this.repositorio.buscarPorEmail(entrada.email);
+
+    if (!usuarioExistente) throw new Error(Erros.USUARIO_NAO_EXISTE)
+
+    const mesmaSenha = this.provedorCripto.comparar(entrada.senha, usuarioExistente.senha!);
+
+    if (!mesmaSenha) throw new Error(Erros.SENHA_INCORRETA)
+
+      return {
+        usuario: { ...usuarioExistente, senha: undefined },
+        token: ''
+      }
+  }
+}
